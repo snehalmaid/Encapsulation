c2d6810ec1181a3383e89e1e3aad7eedd05d018d
snehalmaid <snehalmaid@mail.usf.edu>
2020-02-04 21:13:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Person
+    {
+        public string Name  // property
+        { get; set; }
+    }
+
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Person myObj = new Person();
+        //    myObj.Name = "Liam";
+        //    Console.WriteLine(myObj.Name);
+        //}
+    }
+    class Animal  // Base class (parent) 
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Pig : Animal  // Derived class (child) 
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Dog : Animal  // Derived class (child) 
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
+
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();  // Create a Animal object
+            Animal myPig = new Pig();  // Create a Pig object
+            Animal myDog = new Dog();  // Create a Dog object
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+        }
+    }
+}
